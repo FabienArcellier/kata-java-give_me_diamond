a6b4f5b068866dc8a5f74162ddac9f7b17a2df96
Fabien Arcellier <farcellier@octo.com>
2016-09-25 17:55:16 +0200

    feat: add null returns in case of diamonds requests is negative or even

--- src/main/java/com/fabientools/Diamond.java
+++ src/main/java/com/fabientools/Diamond.java
@@ -2,6 +2,18 @@ package com.fabientools;
 
 public class Diamond {
     public static String print(int n) {
-        return "\n";
+        // Pre-conditions
+        if (n % 2 == 0 || n < 0) {
+            return null;
+        }
+
+        // Processing
+        StringBuffer buffer = new StringBuffer();
+        if (n == 1) {
+            buffer.append("*");
+        }
+
+        buffer.append("\n");
+        return buffer.toString();
     }
 }
--- src/test/java/com/fabientools/DiamondTest.java
+++ src/test/java/com/fabientools/DiamondTest.java
@@ -3,15 +3,29 @@ package com.fabientools;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class DiamondTest {
 
     @Test
-    public void testDiamond0() {
+    public void print_with_1_diamond_returns_one_star() {
         StringBuffer expected = new StringBuffer();
-        expected.append("\n");
+        expected.append("*\n");
 
-        assertEquals(expected.toString(), Diamond.print(0));
+        assertEquals(expected.toString(), Diamond.print(1));
+    }
+
+    @Test
+    public void print_returns_null_when_diamond_number_is_even() {
+        assertNull(Diamond.print(0));
+        assertNull(Diamond.print(2));
+        assertNull(Diamond.print(4));
+    }
+
+    @Test
+    public void print_returns_null_when_diamond_number_is_negative() {
+        assertNull(Diamond.print(-1));
+        assertNull(Diamond.print(-3));
     }
 
     @Test
